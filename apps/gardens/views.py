577dbbf3f3f7af9400d21ae648eb4b47ce2c6cb7
lacsar712 <liug07115@gmail.com>
2026-09-25 16:51:16 +0800

    Initial TeaWither Bug09 GSB snapshot (batch half commit).

---
+++ apps/gardens/views.py
@@ -0,0 +1,217 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    ListView,
+    UpdateView,
+)
+
+from .forms import GardenForm, TroughForm, WitherBatchForm
+from .models import Garden, Trough, WitherBatch
+
+
+def _wants_htmx(request):
+    return request.headers.get("HX-Request") == "true"
+
+
+@login_required
+def home(request):
+    context = {
+        "garden_count": Garden.objects.count(),
+        "trough_count": Trough.objects.count(),
+        "batch_count": WitherBatch.objects.count(),
+        "ready_count": Trough.objects.filter(status=Trough.STATUS_READY).count(),
+        "withering_count": Trough.objects.filter(
+            status=Trough.STATUS_WITHERING
+        ).count(),
+        "loading_count": Trough.objects.filter(
+            status=Trough.STATUS_LOADING
+        ).count(),
+    }
+    return render(request, "home.html", context)
+
+
+# ---- Garden ----
+
+
+class GardenListView(LoginRequiredMixin, ListView):
+    model = Garden
+    template_name = "gardens/list.html"
+    context_object_name = "gardens"
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        if _wants_htmx(request):
+            html = render_to_string(
+                "gardens/_table.html",
+                {"gardens": self.object_list},
+                request=request,
+            )
+            return HttpResponse(html)
+        return super().get(request, *args, **kwargs)
+
+
+class GardenCreateView(LoginRequiredMixin, CreateView):
+    model = Garden
+    form_class = GardenForm
+    template_name = "gardens/form.html"
+    success_url = reverse_lazy("garden_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "茶园已创建")
+        response = super().form_valid(form)
+        if _wants_htmx(self.request):
+            return redirect("garden_list")
+        return response
+
+
+class GardenUpdateView(LoginRequiredMixin, UpdateView):
+    model = Garden
+    form_class = GardenForm
+    template_name = "gardens/form.html"
+    success_url = reverse_lazy("garden_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "茶园已更新")
+        return super().form_valid(form)
+
+
+class GardenDeleteView(LoginRequiredMixin, DeleteView):
+    model = Garden
+    template_name = "gardens/confirm_delete.html"
+    success_url = reverse_lazy("garden_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "茶园已删除")
+        return super().form_valid(form)
+
+
+# ---- Trough ----
+
+
+class TroughListView(LoginRequiredMixin, ListView):
+    model = Trough
+    template_name = "troughs/list.html"
+    context_object_name = "troughs"
+
+    def get_queryset(self):
+        return Trough.objects.select_related("garden").all()
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        if _wants_htmx(request):
+            html = render_to_string(
+                "troughs/_table.html",
+                {"troughs": self.object_list},
+                request=request,
+            )
+            return HttpResponse(html)
+        return super().get(request, *args, **kwargs)
+
+
+class TroughCreateView(LoginRequiredMixin, CreateView):
+    model = Trough
+    form_class = TroughForm
+    template_name = "troughs/form.html"
+    success_url = reverse_lazy("trough_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "萎凋槽已创建")
+        return super().form_valid(form)
+
+
+class TroughUpdateView(LoginRequiredMixin, UpdateView):
+    model = Trough
+    form_class = TroughForm
+    template_name = "troughs/form.html"
+    success_url = reverse_lazy("trough_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "萎凋槽已更新")
+        return super().form_valid(form)
+
+
+class TroughDeleteView(LoginRequiredMixin, DeleteView):
+    model = Trough
+    template_name = "troughs/confirm_delete.html"
+    success_url = reverse_lazy("trough_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "萎凋槽已删除")
+        return super().form_valid(form)
+
+
+# ---- WitherBatch ----
+
+
+class BatchListView(LoginRequiredMixin, ListView):
+    model = WitherBatch
+    template_name = "batches/list.html"
+    context_object_name = "batches"
+
+    def get_queryset(self):
+        return WitherBatch.objects.select_related("trough", "trough__garden").all()
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        if _wants_htmx(request):
+            html = render_to_string(
+                "batches/_table.html",
+                {"batches": self.object_list},
+                request=request,
+            )
+            return HttpResponse(html)
+        return super().get(request, *args, **kwargs)
+
+
+class BatchCreateView(LoginRequiredMixin, CreateView):
+    model = WitherBatch
+    form_class = WitherBatchForm
+    template_name = "batches/form.html"
+    success_url = reverse_lazy("batch_list")
+
+    def form_valid(self, form):
+        # BUG: 先落库再校验；异常吞掉仍当成功
+        self.object = form.save(commit=False)
+        self.object.save()
+        try:
+            self.object.full_clean()
+        except Exception:
+            messages.error(self.request, "校验失败（残行可能已在列表）")
+            return redirect("batch_list")
+        messages.success(self.request, "萎凋批次已创建")
+        return redirect(self.success_url)
+
+
+class BatchUpdateView(LoginRequiredMixin, UpdateView):
+    model = WitherBatch
+    form_class = WitherBatchForm
+    template_name = "batches/form.html"
+    success_url = reverse_lazy("batch_list")
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        try:
+            self.object.full_clean()
+        except Exception:
+            messages.error(self.request, "校验失败")
+            return redirect("batch_list")
+        messages.success(self.request, "萎凋批次已更新")
+        return redirect(self.success_url)
+
+
+class BatchDeleteView(LoginRequiredMixin, DeleteView):
+    model = WitherBatch
+    template_name = "batches/confirm_delete.html"
+    success_url = reverse_lazy("batch_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "萎凋批次已删除")
+        return super().form_valid(form)
